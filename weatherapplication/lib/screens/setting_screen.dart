1829a4589532668a79b2b290cf0040ca4c60d530
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-06-09 11:15:21 +0630

    camera function

--- weatherapplication/lib/screens/setting_screen.dart
+++ weatherapplication/lib/screens/setting_screen.dart
@@ -1,7 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  File? _image;
+
+  Future<void> _pickImage(ImageSource source) async {
+    if (source == ImageSource.camera) {
+      final permissionStatus = await Permission.camera.request();
+      if (permissionStatus != PermissionStatus.granted) {
+        print("Camera granted");
+        return;
+      }
+    }
+
+    final pickedFile = await ImagePicker().pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,9 +40,16 @@ class SettingsScreen extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: 20),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/profileImage.jpeg'),
+            GestureDetector(
+              onTap: () {
+                _showPicker(context);
+              },
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: _image != null
+                    ? FileImage(_image!)
+                    : AssetImage('assets/profileImage.jpeg') as ImageProvider,
+              ),
             ),
             SizedBox(height: 10),
             Text(
@@ -51,12 +86,45 @@ class SettingsScreen extends StatelessWidget {
             Divider(),
             ListTile(
               leading: Icon(Icons.logout),
-              title: Text('Logout'),
+              title: Text(
+                'Logout',
+                style:
+                    TextStyle(fontWeight: FontWeight.normal, color: Colors.red),
+              ),
               onTap: () {},
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _showPicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+              child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.photo_library),
+                title: Text('Gallery'),
+                onTap: () {
+                  _pickImage(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_camera),
+                title: Text('Camera'),
+                onTap: () {
+                  //print('Camera Tapped');
+                  _pickImage(ImageSource.camera);
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          ));
+        });
   }
 }
